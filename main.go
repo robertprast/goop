8a2819ec36228e0a04af704ab4c6fce388337ca7
--replace-all <rmp452@nyu.edu>
2024-10-19 00:47:49 -0400

    listen on all

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	proxyHandler := proxy.NewProxyHandler()
 	http.Handle("/", proxyHandler)
 	logrus.Info("Starting proxy server on :8080")
-	logrus.Fatal(http.ListenAndServe("localhost:8080", nil))
+	logrus.Fatal(http.ListenAndServe(":8080", nil))
 }
